3352266899db0c3c0775f72647f2a800bc9524a4
tnamIT299 <trinhthanhnam2003@gmail.com>
2023-11-01 21:17:39 +0700

    testgit

--- app/src/main/java/com/example/weatherapplication/MainActivity.java
+++ app/src/main/java/com/example/weatherapplication/MainActivity.java
@@ -102,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
                             txt_wind.setText(gio+"m/s");
 
 
+
                             //Đpc dữ liệu từ jsonObject cloud
                             JSONObject jsonObjectCloud = jsonObject.getJSONObject("clouds");
                             String may = jsonObjectCloud.getString("all");
